18a11b8898f3a73e0a4898ba6e02cb6f7e2d9bb1
Gustavo Alarcón <galarcon@meraki-s.com>
2019-04-14 04:58:36 -0500

    Feature done - User edit password
     * Feature - Dialog for update password
     * Feature - Cloud function to update the password saved
    * Enhancement - Password visibility at login

---
+++ src/app/main/confirm-change-password/confirm-change-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-change-password',
+  templateUrl: './confirm-change-password.component.html',
+  styles: []
+})
+export class ConfirmChangePasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
